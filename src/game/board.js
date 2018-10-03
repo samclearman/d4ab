d35a14d588b5416a2a581e7f7692117055a4af49
Akshat Bubna <akshat@scaleapi.com>
2018-10-02 22:19:54 -0700

    oops

--- src/game/board.js
+++ src/game/board.js
@@ -110,7 +110,7 @@ export const hasValidMove = (b, player) => {
 
 export const getNextPlayer = (b, player) => {
   for (const i of _.range(b.settings.players - 1)) {
-    const nextPlayer = (player + i) % b.settings.players
+    const nextPlayer = (player + i + 1) % b.settings.players
     if (hasValidMove(b, nextPlayer)) {
       return nextPlayer
     }
